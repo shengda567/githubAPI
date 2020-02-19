ece798d69009f6e0e0afe56d4463ac03c13b7af9
shengda567 <57602285+shengda567@users.noreply.github.com>
2020-02-18 21:49:31 -0500

    Update gitHubAPI.py

--- gitHubAPI.py
+++ gitHubAPI.py
@@ -2,22 +2,39 @@ import requests
 import json
 
 
-def gitHubAPI(username):
+class GitHubAPI:
+    def __init__(self, username):
+        self.username = username
 
-    response = requests.get(f"https://api.github.com/users/{username}/repos")
+    def get_response(self):
+        response = requests.get(f"https://api.github.com/users/{self.username}/repos")
+        return response
 
-    data = response.json()
-    list = []
-    for repo in data:
-        try:
+    def get_repo_commit_dict(self):
+        data = self.get_response().json()
+
+        dict = {}
+        for repo in data:
             repository_name = repo["name"]
-        except(TypeError, KeyError, IndexError):
-            return "invalid name"
-        else:
-            commits = requests.get(f"https://api.github.com/repos/{username}/{repository_name}/commits")
+            commits = requests.get(f"https://api.github.com/repos/{self.username}/{repository_name}/commits")
             commits_number = len(commits.json())
-            output = f"Repo: {username} Number of commits : {commits_number}"
-            list.append(output)
-    return list
+            dict[repository_name] = commits_number
+        #print(dict)
+        return dict
+
+    def print_data(self):
+        dict = self.get_repo_commit_dict()
+        for repo_name, repo_commits in dict.items():
+            print(f"Repo: {repo_name} Number of commits : {repo_commits}")
+
+    def response_valid(self):
+        return self.get_response().status_code == 200
+
+def main():
+    username = input("input your username: ")
+    GitHubAPI(f"{username}").print_data()
+    #print(GitHubAPI("adfadfasdfsdfegrtyfgdsg").get_response())
+
+
 if __name__ == '__main__':
-    gitHubAPI("shengda567")
+    main()
