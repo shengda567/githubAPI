eeafadb5cd868a915417968514c076e171b8b0a3
shengda567 <57602285+shengda567@users.noreply.github.com>
2020-02-18 21:48:35 -0500

    Update TestGitHubAPI.py

--- TestGitHubAPI.py
+++ TestGitHubAPI.py
@@ -1,20 +1,22 @@
 import unittest
 
-from .gitHubAPI import gitHubAPI
+from gitHubAPI import GitHubAPI
 
 class TestGitHubAPI(unittest.TestCase):
-    def test_commit_results(self):
-        expect = ["Repo: shengda567 Number of commits : 2",
-                  "Repo: shengda567 Number of commits : 1",
-                  "Repo: shengda567 Number of commits : 2",
-                  "Repo: shengda567 Number of commits : 1",
-                  "Repo: shengda567 Number of commits : 3",
-                  "Repo: shengda567 Number of commits : 2",
-                  "Repo: shengda567 Number of commits : 7"]
-        self.assertEqual(gitHubAPI("shengda567"), expect)
+    def setUp(self):
+        self.repo = GitHubAPI("shengda567")
 
-    def test_invalid_name(self):
-        self.assertEqual(gitHubAPI('asdfasdf'), "invalid name")
+    def test_repo_names(self):
+        expect = ["222", "555-Project", "githubAPI", "hello-world",
+                  "python810zsd", "SSW567A", "STpython810_repository", "Triangle567"]
+        actual = []
+        for repo_names in self.repo.get_repo_commit_dict():
+            actual.append(repo_names)
+        self.assertEqual(actual, expect)
+
+    def test_valid_response(self):
+        self.assertEqual(GitHubAPI('invalidinput1234567').response_valid(), False)
+        self.assertEqual(GitHubAPI('shengda567').response_valid(), True)
 
 if __name__ == '__main__':
     print('Running unit tests for GitHubAPI')
